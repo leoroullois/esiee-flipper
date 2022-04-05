013210164029c7e137fa888becd6c6fcb8266cc5
leoroullois <roullois.leo@gmail.com>
2022-04-05 18:18:35 +0200

    feat: border flash

--- Eleve.cpp
+++ Eleve.cpp
@@ -50,81 +50,86 @@ float dist(V2 A, V2 B, V2 P) {
 // }
 
 struct Bumper {
-  float x, y, r;
-  float temps;
-  bool visible;
-  int etat;
+  float x, y, r, time;
+  bool visible, collision;
+  int state;
 
   Bumper(float _x, float _y, float _r, bool _visible) {
     x = _x;
     y = _y;
     r = _r;
     visible = _visible;
-    temps = 0;
-    etat = 0;
+    collision = false;
+    time = 0;
+    state = 0;
   }
-  void setTemps(void) {
+  void setTime(float _time) { time = _time; }
+  float getTime(void) { return time; }
+  void setState(int _state) { state = _state; }
+  bool getCollision(void) { return collision; }
+  void setCollision(bool _collision) { collision = _collision; }
+
+  V2 getV2(void) { return V2(x, y); }
+  void drawBumper(void) { G2D::DrawCircle(getV2(), r, Color::Blue, visible); }
+
+  void setAndDrawState(float time, bool _collision) {
     float T = 2.0 / 4;
     float dt = G2D::ElapsedTimeFromLastCallbackSeconds();
-    temps += dt;
-    std::cout << "temps : " << temps;
-    std::cout << " dt : " << dt << std::endl;
-    if (temps + dt >= 4 * T) {
-      temps = 0;
-      etat = 4;
-      drawEtat();
-    } else if (temps + dt >= 3 * T) {
-      setEtat(3);
-      drawEtat();
-    } else if (temps + dt >= 2 * T) {
-      // temps += dt;
-      setEtat(2);
-      drawEtat();
-    } else if (temps + dt >= T) {
-      // temps += dt;
-      setEtat(1);
-      drawEtat();
 
-    } else {
-      // temps += dt;
-      setEtat(0);
-      drawEtat();
+    if ((time == 0 && _collision) || time != 0) {
+
+      std::cout << "time : " << time << "collision " << _collision << std::endl;
+      setTime(time + dt);
+      if (time >= 4 * T) {
+        setTime(0);
+        setState(0);
+      } else if (time >= 3 * T) {
+        setState(1);
+        drawCurrentState();
+      } else if (time >= 2 * T) {
+        setState(2);
+        drawCurrentState();
+      } else if (time >= T) {
+        setState(3);
+        drawCurrentState();
+      } else {
+        setState(4);
+        drawCurrentState();
+      }
     }
   }
+
   void drawBorder(int rayon) {
     G2D::DrawCircle(getV2(), r + rayon, Color::Red, true);
     G2D::DrawCircle(getV2(), r, Color::Blue, true);
   }
-  void drawEtat() {
-    switch (etat) {
+  void drawCurrentState() {
+    switch (state) {
     case 0:
-      drawBorder(20);
+      drawBorder(0);
       break;
     case 1:
-      drawBorder(15);
+      drawBorder(5);
       break;
     case 2:
       drawBorder(10);
       break;
     case 3:
-      drawBorder(5);
+      drawBorder(15);
       break;
     case 4:
-      drawBorder(0);
+      drawBorder(20);
       break;
     default:
       break;
     }
   }
-  void setEtat(int _etat) { etat = _etat; }
-
-  V2 getV2(void) { return V2(x, y); }
-  void drawBumper(void) { G2D::DrawCircle(getV2(), r, Color::Blue, visible); }
 };
 
 // information de partie
 struct GameData {
   int idFrame = 0;
+  bool collision = false;
   int HeighPix = 800;
   int WidthPix = 600;
   V2 BallPos = V2(550, 30);
@@ -152,9 +157,9 @@ V2 rotate(V2 v, float angle) {
   float x = v.x * cos(angle) - v.y * sin(angle);
   float y = v.x * sin(angle) + v.y * cos(angle);
   V2 vbis = V2(x, y);
-  std::cout << "alpha=" << angle * 180 / PI << "°";
-  std::cout << " v=" << v;
-  std::cout << " v'=" << vbis << endl;
+  // std::cout << "alpha=" << angle * 180 / PI << "°";
+  // std::cout << " v=" << v;
+  // std::cout << " v'=" << vbis << endl;
   return vbis;
 }
 
@@ -178,8 +183,13 @@ void render() {
   G2D::DrawCircle(G.BallPos, 15, Color::Red, true);
 
   for (int i = 0; i < 3; i++) {
-    // G.bumpers[i].drawBumper();
-    G.bumpers[i].setTemps();
+    Bumper &bumper = G.bumpers[i];
+    bumper.drawBumper();
+    if (bumper.collision) {
+      bumper.setAndDrawState(0, true);
+    } else {
+      bumper.setAndDrawState(bumper.getTime(), false);
+    }
   }
 
   for (int i = 0; i < 11; i++)
@@ -228,9 +238,6 @@ void handleBumperCollision(Bumper &bumper, float x, float y) {
     float ny = bumper.y - G.BallPos.y;
 
     G.BallSpeed = Rebond(G.BallSpeed, V2(nx, ny));
-    float dt = G2D::ElapsedTimeFromLastCallbackSeconds() * 1e3;
-    // if(bumper)
-    bumper.setTemps();
   }
 }
 void handleRoundedBorder(void) {
@@ -238,21 +245,42 @@ void handleRoundedBorder(void) {
   }
 }
 
-void handleBumperBorder(Bumper &bumper) {}
 void handleBumpers(float x, float y) {
   // ? pour chacun des bumpers
   for (int i = 0; i < 3; i++) {
     Bumper &bumper = G.bumpers[i];
     handleBumperCollision(bumper, x, y);
-    // TODO: handleBumperBorder(bumper);
   }
 }
-
+/**
+ * @brief synchroniser la collision des bumper entre la fonction logique et
+ * render pour déclencher le flash
+ */
+void synchronizeBumperCollision(void) {
+  if (detectCircleCollision(G.BallPos, G.bumper1)) {
+    G.bumpers[0].collision = true;
+    G.bumpers[1].collision = false;
+    G.bumpers[2].collision = false;
+  } else if (detectCircleCollision(G.BallPos, G.bumper2)) {
+    G.bumpers[0].collision = false;
+    G.bumpers[1].collision = true;
+    G.bumpers[2].collision = false;
+  } else if (detectCircleCollision(G.BallPos, G.bumper3)) {
+    G.bumpers[0].collision = false;
+    G.bumpers[1].collision = false;
+    G.bumpers[2].collision = true;
+  } else {
+    G.bumpers[0].collision = false;
+    G.bumpers[1].collision = false;
+    G.bumpers[2].collision = false;
+  }
+}
 void Logic() {
   G.idFrame += 1;
   float x = G.BallPos.x;
   float y = G.BallPos.y;
   nextMove();
+  synchronizeBumperCollision();
   handleWall(x, y);
   handleBumpers(x, y);
 }
